00d9f4f42e2bc52d28ab587c2a27640fec043166
TotallyNotMalicious <LCMLegacy12@protonmail.com>
2021-01-25 17:22:17 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Description
+currency conversion from usd:eur, as well as eur:usd
+
+# How To Run
+```
+g++ -o main main.cpp
+./main (on linux)
+main   (on windows, or just run the executable)
+```
---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+
+    cout << "Enter your choice\n\n1: Convert USD to EUR\n2: Convert EUR to USD\n\nOption: ";
+    cin >> choice;
+
+    switch(choice) {
+        case 1:
+            int usd;
+
+            cout << "USD: ";
+            cin >> usd;
+            cout << "EUR: " << usd * 0.82 << "\n";
+            break;
+        case 2:
+            int eur;
+
+            cout << "EUR: ";
+            cin >> eur;
+            cout << "USD: " << eur * 1.21 << "\n";
+            break;
+        default:
+            cout << "Invalid choice. Please choose either 1 or 2\n";
+            break;
+    }
+    system("pause");
+}
